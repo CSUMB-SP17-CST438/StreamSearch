edf336ce2d9cf1486a86acac119a9ded1030e628
Tala Huhe <tala.huhe@gmail.com>
2017-02-14 23:22:32 +0000

    Socket.io wire

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 import os
 import flask
+import flask_socketio
 
 app = flask.Flask(__name__)
+socketio = flask_socketio.SocketIO(app) 
 
 @app.route('/')
 def hello():
     return flask.render_template('index.html')
 
-app.run(
+@socketio.on('connect') 
+def on_connect():
+    print 'Someone connected!'
+    
+@socketio.on('disconnect')
+def on_disconnect():
+    print 'Someone disconnected!'
+    
+@socketio.on('new number')
+def on_new_number(data):
+    print "Got an event for new number with data:", data
+    # TODO: Fill me out!
+
+socketio.run(
+    app,
     host=os.getenv('IP', '0.0.0.0'),
     port=int(os.getenv('PORT', 8080)),
     debug=True
 )
+
--- scripts/Content.js
+++ scripts/Content.js
@@ -1,7 +1,33 @@
 import * as React from 'react';
 
+import { Button } from './Button';
+import { Socket } from './Socket';
+
 export class Content extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'numbers': []
+        };
+    }
+    componentDidMount() {
+        Socket.on('all numbers', (data) => {
+            this.setState({
+                'numbers': data['numbers']
+            });
+        })        
+    }
+
     render() {
-        return <div><h1>Hello from React!</h1></div>;
+        let numbers = this.state.numbers.map(
+            (n, index) => <li key={index}>{n}</li>
+        );
+        return (
+            <div>
+                <h1>Random numbers so far!</h1>
+                <ul>{numbers}</ul>
+                <Button />
+            </div>
+        );
     }
 }
