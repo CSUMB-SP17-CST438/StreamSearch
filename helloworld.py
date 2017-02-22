5e5ef532ecf3707347033aa210617b3ef1e1e3ba
elgomez <elgomez@csumb.edu>
2017-02-22 03:04:09 +0000

    commit before trying something new

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+from random import randint
+for i in range (10):
+    x = randint(0,50)
+    j = i+1
+    print j, x
---
+++ test2/main.py
@@ -0,0 +1,6 @@
+
+# main.py
+import strings
+import super_secret_random as ssr
+print strings.get_hello()
+print ssr.get_number()
