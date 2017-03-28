0af0f9b00da5e225a31c762b78a2ec91928d8753
brstillwell <brstillwell@csumb.edu>
2017-03-28 04:27:56 +0000

    fixed the bundle.js error by changing path in index.html & webpack.config.js, also changed name to script.js

--- app.py
+++ app.py
@@ -2,24 +2,26 @@ import os
 import flask
 import flask_socketio
 import requests
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = flask.Flask(__name__)
 socketio = flask_socketio.SocketIO(app)
 
 #Guidebox API Key c338d925a0672acf243133ddc1d5d66fb0191391
-@socketio.on('connect')
-def on_connect():
-    test()
-    
-def test():
-    socketio.emit('hello to client', {
-        'message': 'Hey there!'
-    })
-    
 @app.route('/')
 def hello():
     return flask.render_template('index.html')
+   
+@socketio.on('connect')
+def on_connect():
+    print('Client connected')
     
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+    
+ 
 if __name__ == '__main__':
     socketio.run(
         app,
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  debug: true,
   entry: [
     './src/index.js'
   ],
   output: {
     path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    filename: './static/script.js'
   },
   module: {
     loaders: [{
