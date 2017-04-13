cda85bb3f8565ca30c606911bcd4bce912637f33
brstillwell <brstillwell@csumb.edu>
2017-04-13 05:41:39 +0000

    reverted react-redux to fix errors

--- node_modules/react-redux/lib/utils/wrapActionCreators.js
+++ node_modules/react-redux/lib/utils/wrapActionCreators.js
@@ -1,12 +1,14 @@
 'use strict';
 
 exports.__esModule = true;
-exports["default"] = wrapActionCreators;
+exports['default'] = wrapActionCreators;
 
 var _redux = require('redux');
 
 function wrapActionCreators(actionCreators) {
   return function (dispatch) {
-    return (0, _redux.bindActionCreators)(actionCreators, dispatch);
+    return _redux.bindActionCreators(actionCreators, dispatch);
   };
 }
+
+module.exports = exports['default'];
