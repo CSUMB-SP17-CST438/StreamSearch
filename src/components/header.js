d987bf24afcc5e9f3cf3fc187fd6d4db06f2cd96
Miguel Lopez <mnl30@aol.com>
2017-04-17 07:02:59 +0000

    moved facebook log-in

--- src/components/header.js
+++ src/components/header.js
@@ -22,13 +22,7 @@ export default class App extends Component {
 			<header className="header">
 				<h1>STREAM SEARCH</h1>
 			        <h2>Let us do the Searching for you!</h2>
-			    <div                  
-                    className="fb-login-button"     
-                    data-max-rows="1"
-                    data-size="medium"
-                    data-show-faces="false" 
-                    data-auto-logout-link="true">
-                </div>
+			    
 			</header>
 		);
 	}
